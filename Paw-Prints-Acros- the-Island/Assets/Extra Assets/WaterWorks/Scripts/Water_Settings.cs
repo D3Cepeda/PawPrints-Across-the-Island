411e3b93d389e434a8835ed1e3492d2d2748efd2
D3Cepeda <d3cepeda@gmail.com>
2025-07-25 22:34:02 -0400

    Fixed a bug of the water in Daniel Sample
    Fixed a bug where the waves of the beach were static if the user was moving. this was done in Daniel Sample Scene

--- Paw-Prints-Acros- the-Island/Assets/Extra Assets/WaterWorks/Scripts/Water_Settings.cs
+++ Paw-Prints-Acros- the-Island/Assets/Extra Assets/WaterWorks/Scripts/Water_Settings.cs
@@ -8,9 +8,9 @@ public class Water_Settings : MonoBehaviour
     Material waterVolume;
     Material waterMaterial;
 
-    void Update()
+    void LateUpdate()
     {
-        if(waterVolume == null)
+        if (waterVolume == null)
         {
             waterVolume = (Material)Resources.Load("Water_Volume");
         }
@@ -20,6 +20,21 @@ public class Water_Settings : MonoBehaviour
             waterMaterial = GetComponent<MeshRenderer>().sharedMaterial;
         }
 
-        waterVolume.SetVector("pos", new Vector4(0, (waterVolume.GetVector("bounds").y / -2) + transform.position.y + (waterMaterial.GetFloat("_Displacement_Amount") / 3), 0, 0));
+        if (waterVolume != null && waterMaterial != null)
+        {
+            Vector4 posValue = new Vector4(
+                0,
+                (waterVolume.GetVector("bounds").y / -2) + transform.position.y + (waterMaterial.GetFloat("_Displacement_Amount") / 3),
+                0,
+                0
+            );
+
+            waterVolume.SetVector("pos", posValue);
+
+#if UNITY_EDITOR
+            // Verificación opcional para confirmar que se está actualizando:
+            Debug.Log("Water Volume Pos actualizado a: " + posValue);
+#endif
+        }
     }
 }
